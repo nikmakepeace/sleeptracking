ed72efff77afda14f1cd12f910ef6704fb1f89a5
Nik <git@nikmakepeace.com>
2022-11-21 17:52:34 +0000

    FIxed the method signatures to include self

--- SleepMaths.py
+++ SleepMaths.py
@@ -2,15 +2,15 @@
 class Aggregator:
     """Class to contain functions that operate on column data and reduce to a single value"""
 
-    def mean(values: list) -> float:
+    def mean(self, values: list) -> float:
         return sum(values) / len(values)
 
-    def min(values: list) -> int:
+    def min(self, values: list) -> int:
         return min(values)
 
-    def max(values: list) -> int:
+    def max(self, values: list) -> int:
         return max(values)
 
-    def count(values: list) -> int:
+    def count(self, values: list) -> int:
         return len(values)
 
