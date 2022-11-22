ebbc2e9174a4e2b8c09e322d81035ea0967db73b
Nik <git@nikmakepeace.com>
2022-11-22 14:28:25 +0000

    Added tests for SleepMaths

--- SleepMaths.py
+++ SleepMaths.py
@@ -3,14 +3,17 @@ class Aggregator:
     """Class to contain functions that operate on column data and reduce to a single value"""
 
     def mean(self, values: list) -> float:
+        """Produces the mean of all the numbers in a list"""
         return sum(values) / len(values)
 
     def min(self, values: list) -> int:
+        """Produces the smallest number of all the numbers in a list"""
         return min(values)
 
     def max(self, values: list) -> int:
+        """Produces the largest number of all the numbers in a list"""
         return max(values)
 
     def count(self, values: list) -> int:
+        """Produces the number of numbers in the list"""
         return len(values)
-
---
+++ tests/test_aggregator.py
@@ -0,0 +1,40 @@
+import unittest
+
+from SleepMaths import Aggregator
+
+
+class TestAggregator(unittest.TestCase):
+    def setUp(self):
+        self.set_1 = {
+            'values': [1, 1, 2, 2, 9, 21],
+            'mean': 6.0,
+            'count': 6,
+            'min': 1,
+            'max': 21
+        }
+        self.set_2 = {
+            'values': [100, 30, 5, 0],
+            'mean': 33.75,
+            'count': 4,
+            'min': 0,
+            'max': 100
+        }
+        self.aggregator = Aggregator()
+
+    def test_mean(self):
+        self.assertEqual(self.set_1['mean'], self.aggregator.mean(self.set_1['values']))
+        self.assertEqual(self.set_2['mean'], self.aggregator.mean(self.set_2['values']))
+
+    def test_count(self):
+        self.assertEqual(self.set_1['count'], self.aggregator.count(self.set_1['values']))
+        self.assertEqual(self.set_2['count'], self.aggregator.count(self.set_2['values']))
+
+    def test_min(self):
+        self.assertEqual(self.set_1['min'], self.aggregator.min(self.set_1['values']))
+        self.assertEqual(self.set_2['min'], self.aggregator.min(self.set_2['values']))
+
+    def test_max(self):
+        self.assertEqual(self.set_1['max'], self.aggregator.max(self.set_1['values']))
+        self.assertEqual(self.set_2['max'], self.aggregator.max(self.set_2['values']))
+
+    # TODO test handling lists with non-numeric items
